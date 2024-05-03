5e901baf02273b2c574c47f1045574139d294e98
Timo <situx@users.noreply.github.com>
2024-05-03 02:05:12 +0200

    fixes

--- linkeddata/pyscripts/fortresses2rdf.py
+++ linkeddata/pyscripts/fortresses2rdf.py
@@ -20,12 +20,12 @@ for rd in roads:
     g.add((URIRef(datans+"fortress"+str(rd["period"])),URIRef("http://www.w3.org/2000/01/rdf-schema#label"),Literal("Fortress "+str(rd["period"]),lang="de")))
     g.add((URIRef(datans+"periods/"+str(rd["period"])),URIRef("http://www.w3.org/2006/time#before"),URIRef(datans+"period/25")))
     g.add((URIRef(datans+"fortress"+str(featcounter)),URIRef("http://www.opengis.net/ont/geosparql#hasGeometry"),URIRef(datans+"fortress"+str(featcounter)+"_geom"))) 
-    g.add((URIRef(datans+"fortress"+str(featcounter)+"_geom"),URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),URIRef("http://www.opengis.net/ont/sf#LineString")))
+    g.add((URIRef(datans+"fortress"+str(featcounter)+"_geom"),URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),URIRef("http://www.opengis.net/ont/sf#Point")))
     g.add((URIRef(datans+"fortress"+str(featcounter)+"_geom"),URIRef("http://www.w3.org/2000/01/rdf-schema#label"),Literal("Fortress "+str(featcounter)+" Geometry",lang="en")))
     g.add((URIRef(datans+"fortress"+str(featcounter)+"_geom"),URIRef("http://www.w3.org/2000/01/rdf-schema#label"),Literal("Fortress "+str(featcounter)+" Geometrie",lang="de")))
     g.add((URIRef(datans+"fortress"+str(featcounter)+"_geom"),URIRef("http://www.opengis.net/ont/geosparql#asWKT"),Literal("<http://www.opengis.net/def/crs/EPSG/0/32628> "+str(rd["WKT"]),datatype="http://www.opengis.net/ont/geosparql#wktLiteral")))  
     featcounter+=1
-g.add((URIRef("http://www.opengis.net/ont/sf#LineString"),URIRef("http://www.w3.org/2000/01/rdf-schema#subClassOf"),URIRef("http://www.opengis.net/ont/geosparql#Geometry")))
+g.add((URIRef("http://www.opengis.net/ont/sf#Point"),URIRef("http://www.w3.org/2000/01/rdf-schema#subClassOf"),URIRef("http://www.opengis.net/ont/geosparql#Geometry")))
 g.add((URIRef(ontns+"Fortress"),URIRef("http://www.w3.org/2000/01/rdf-schema#subClassOf"),URIRef("http://www.opengis.net/ont/geosparql#Feature")))
 g.serialize("fortresses.ttl")
     
